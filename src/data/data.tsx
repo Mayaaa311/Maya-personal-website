bbfd8482eae487a8cefa251e2b1d342359fe10c4
Maya Yuan <mayaaa@umich.edu>
2023-12-02 04:35:59 -0500

    myvers

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -9,11 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 
 import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
-import heroImage from '../images/header-background.webp';
+import heroImage from '../images/header-background.jpg';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
@@ -26,7 +23,7 @@ import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic from '../images/profilepic.jpg';
-import testimonialImage from '../images/testimonial.webp';
+import testimonialImage from '../images/testimonial.jpg';
 import {
   About,
   ContactSection,
@@ -44,8 +41,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: "Mayas website",
+  description: "Maya's website",
 };
 
 /**
@@ -69,24 +66,19 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Yining Yuan.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
-      </p>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        
+UMICH CSE '23 | MDP Intern @ ProQuest,
+Currently looking for summer internship!
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: 'assets/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -104,16 +96,17 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `I'm a rising Senior at the University of Michigan in Computer Science, 
+  graduating in December 2023. Specializing in machine learning, 
+  backend software development, and data analysis, I am planning to continue my education in gradschool and I'm currently 
+  seeking a spring/summer internship opportunity. `,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Ann Arbor, MI', Icon: MapIcon},
+    {label: 'Age', text: '21', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Chinese', Icon: FlagIcon},
+    {label: 'Interests', text: 'Piano, guitar, singing', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Michigan, Ann Arbor', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'part time instructional assistant @ UMich', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -125,16 +118,12 @@ export const skills: SkillGroup[] = [
     name: 'Spoken languages',
     skills: [
       {
-        name: 'English',
+        name: 'Chinese',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
+        name: 'English',
+        level: 9,
       },
     ],
   },
@@ -143,14 +132,10 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'React',
-        level: 9,
+        level: 6,
       },
       {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
+        name: 'Javascript',
         level: 6,
       },
     ],
@@ -159,33 +144,79 @@ export const skills: SkillGroup[] = [
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
+        name: 'RESTful API',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
+        name: 'mySQL',
+        level: 7,
       },
       {
-        name: 'Golang',
-        level: 4,
+        name: 'flask',
+        level: 7,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Machine Learning',
     skills: [
       {
-        name: 'React Native',
+        name: 'Scikit learn',
+        level: 8,
+      },
+      {
+        name: 'numpy',
+        level: 8,
+      },
+      {
+        name: 'Pytorch',
+        level: 7,
+      },
+      {
+        name: 'XG Boost',
+        level: 7,
+      },
+      {
+        name: 'Amazon Sagemaker',
+        level: 7,
+      },
+    ],
+  },
+  {
+    name: 'Programming languages',
+    skills: [
+      {
+        name: 'C++',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Python',
+        level: 8,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'C',
+        level: 6,
+      },
+      {
+        name: 'Java',
+        level: 5,
+      },
+    ],
+  },
+  {
+    name: 'Project management',
+    skills: [
+      {
+        name: 'git version control',
+        level: 8,
+      },
+      {
+        name: 'software development cycle',
+        level: 7,
+      },
+      {
+        name: 'software development principles',
+        level: 7,
       },
     ],
   },
@@ -268,40 +299,87 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'Dec 2023',
+    location: 'University of Michigan, Ann Arbor',
+    title: 'Bachelor of Computer Science Engineering',
+    content: <p>Complete my degree with GPA over 3.8, University honor for multiple semester. Taken courses
+     related to data structure and algorithms, computer vision, machine learning, database management systems, 
+     web system, and human centered software engineering.
+    </p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'Jan 2020 - Present',
+    location: 'Proquet & University of Michigan',
+    title: 'Multidisciplinary Design Project Intern',
+    content: (
+        <>
+        <p>
+          Worked on an end-to-end collaborative machine learning project that predicted the success of newly emerging topics 
+          for after a decade, using the ProQuest dissertation database and Web of Science database data. Developed a 
+          model incorporating a feature extraction system, feature engineering system, truth label engineering system,
+          and a model training and prediction system. Currently able to predict with a precision at the top 200 for an around 50% accuracy.
+        </p>
+        <br></br>
+        <p>
+          Utilized Python as our development programming language. Managed the database using dask and pandas dataframe. 
+          In this project, I worked extensively with machine learning packages such as seaborn, scikit-learn, PyTorch, and matplotlib,
+          as well as data preprocessing packages like numpy and math. Gained valuable experience working with AWS virtual machines,
+          managing teamwork effectively, and handling large-scale datasets. This project not only enhanced my technical skills in machine
+          learning and data processing but also developed my abilities in collaborative teamwork and big data management.
+        </p>
+      </>
+    ),
+  },
+  {
+    date: 'Aug 2023 - Dec 2023',
+    location: 'Unviersity of Michigan',
+    title: 'Instructional Assistant for Applied Machine Learning',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Conducted weekly instructor team meetings, managed and edited class materials, created homework and midterm questions, graded assignments, and provided student support during office hours.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'Aug 2022 - Dec 2022',
+    location: 'Unviersity of Michigan',
+    title: 'Instructional Assistant for Computational Linear Algebra',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+        Led a beginner programming study group once a week, met with my team once a week and designed a study group agenda.
+Reviewed and helped the professor with editing the ROB101 textbook during summer using latex, wrote the section that explained and compared Julia(the programming language used in class) to C++.
+Hosted office hours and lab sessions once a week, and graded student’s homework coding assignment.
+Meet and communicate with instructors once a week, discussing about teaching strategy and students performance in class.
+    </p>
+    ),
+  },
+  {
+    date: 'Jan 2022 - July 2022',
+    location: 'Unviersity of Michigan',
+    title: 'Western Forest and Fire Initiative Research Assistant',
+    content: (
+      <p>
+        A research project aimed to analyze the relationship between stakeholders in western forest
+         wildfire management using data from https://inciweb.nwcg.gov/ website. Implemented an automated 
+         web scraping program that collected various types of fire incident data from over 2000 web pages
+          using the bs web-scraper, developed a database of actors involved in wildfire management using
+           pandas dataframe. Preprocessed data using NLP model, extracting named-entities from overview 
+           section, providing critical data for future analysis.</p>
+    ),
+  },
+  {
+    date: 'Aug 2021 - Dec 2021',
+    location: 'Unviersity of Michigan',
+    title: 'Analyzing the National Covid Data Cohort Research Assistant',
+    content: (
+      <p>
+        Trained three machine learning models on data from the National COVID Cohort Collaborative, 
+        predicted COVID severity in pediatric patients with an impressive 82% AUROC. Acquire hands-on 
+        experience with essential Python libraries such as sklearn, pandas, and numpy. Project help with exploring
+        and experimenting machine learning applications within a critical healthcare context in a newly developed data corhort.</p>
     ),
   },
 ];
@@ -341,11 +419,11 @@ export const contact: ContactSection = {
     {
       type: ContactType.Email,
       text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      href: 'mailto:mayaaa@umich.edu',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
+      text: 'Ann Arbor, Michigan',
       href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
     },
     {
@@ -356,7 +434,7 @@ export const contact: ContactSection = {
     {
       type: ContactType.Github,
       text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      href: 'https://github.com/Mayaaa311',
     },
   ],
 };
@@ -365,9 +443,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/Mayaaa311'},
+  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/yining-yuan-7065aa224/'},
+  // {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
+  // {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
 ];
